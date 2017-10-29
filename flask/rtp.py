254cbc4db54130633826454ca96fd1102262a210
rainbowjlinux <rainbowjlinux@gmail.com>
2017-10-30 00:55:52 +0800

    understand the logic of render_template

--- flask/rtp.py
+++ flask/rtp.py
@@ -2,9 +2,9 @@ from flask import render_template, Flask
 
 app = Flask(__name__)
 
-@app.route('/usr/<usrname>')
-def usr_index(usrname):
-    return render_template('usr_index.html', usrname=usrname)
+@app.route('/user/<username>')
+def user_index(username):
+    return render_template('user_index.html', username=username)
 
 if __name__ == '__main__':
     app.run()
